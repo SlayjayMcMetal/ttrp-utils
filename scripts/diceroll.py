00bdad4ea07f782d2b9915fc2f4e7915e4ddd4e7
SlayjayMcMetal <primeministeraj@gmail.com>
2025-04-25 16:43:15 -0400

    added if statements and functions

--- scripts/diceroll.py
+++ scripts/diceroll.py
@@ -1,9 +1,29 @@
 import random 
-print("welcome to duh dice boi")
-sides = int(input("How many die sides?"))
-num_dice = int(input("How many num_dice?"))
-for i in range(num_dice):
-    print(f"rolling a d{sides}...")
-    result=random.randint(1,sides)
-    print(f"{result}")
+def roll_dice(sides,num_dice):
+    total=0
+    for i in range(num_dice):
+        result=random.randint(1,sides)
+        total+=result
+    return total
 
+
+def cast_fireball(wisdom):
+    print("Casting fireball")
+    damage=roll_dice(6,10)+wisdom
+    print(f"The fire did {damage} damage")
+
+
+
+
+#print("welcome to duh dice boi")
+#sides = int(input("How many die sides?"))
+#num_dice = int(input("How many num_dice?"))
+#result=roll_dice(sides,num_dice)
+#print(f"Roll result is {result}")
+
+wisdom = int(input("What is yer wisdom?"))
+user_choice = input("Would you like to cast fireball? [y/n]")
+if user_choice == "y":
+    cast_fireball(wisdom)
+else:
+    print("Aight then")
